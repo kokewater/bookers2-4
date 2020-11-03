83114ce19fa9cf99b43df900ceca5b313438f103
EC2 Default User <ec2-user@ip-172-31-10-26.ec2.internal>
2020-11-03 09:17:03 +0000

    [Add]relationshipモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  attachment :profile_image, destroy: false
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  attachment :profile_image, destroy: false
+  
+  # ====================自分がフォローしているユーザーとの関連 ===================================
+  #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。親はfollowed_id(フォローする側)
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :followed_id
+  # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「followings」と定義
+  has_many :followings, through: :active_relationships, source: :follower
+  
+  
+  # ====================自分がフォローされるユーザーとの関連 ===================================
+  #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。親はfollower_id(フォローされる側)
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # 中間テーブルを介して「followed」モデルのUser(フォローする側)を集めることを「followers」と定義
+  has_many :followers, through: :passive_relationships, source: :followed
+  
+  def followed_by?
+    passive_relationships.find_by(followed_id: user.id).present?
+  end
+  
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50 }
