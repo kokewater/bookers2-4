1706f77ed511b62cef7ca8263b17a26b6841d0fc
EC2 Default User <ec2-user@ip-172-31-10-26.ec2.internal>
2020-11-04 12:03:52 +0000

    [Fix]dependent: :destroy追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,14 @@ class User < ApplicationRecord
   
   # ====================自分がフォローしているユーザーとの関連 ===================================
   #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。親はfollowed_id(フォローする側)
-  has_many :active_relationships, class_name: "Relationship", foreign_key: :followed_id
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
   # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「followings」と定義
   has_many :followings, through: :active_relationships, source: :follower
   
   
   # ====================自分がフォローされるユーザーとの関連 ===================================
   #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。親はfollower_id(フォローされる側)
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   # 中間テーブルを介して「followed」モデルのUser(フォローする側)を集めることを「followers」と定義
   has_many :followers, through: :passive_relationships, source: :followed
   
